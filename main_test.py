74d41282c96c8fcfc17328d07a2d0c1ae23b7217
Trevor Bloomfield <bloomfieldtm@gmail.com>
2023-05-21 19:06:20 -0500

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -2,6 +2,8 @@ import unittest
 import os
 from main import WordLinkGenerator
 from io import StringIO
+from contextlib import redirect_stdout
+
 
 
 class WordLinkGeneratorTest(unittest.TestCase):
@@ -19,8 +21,10 @@ class WordLinkGeneratorTest(unittest.TestCase):
     def test_find_word_locations(self):
         generator = WordLinkGenerator('test', self.test_dir, None)
         locations = generator.find_word_locations(self.file_path)
-        expected_locations = [(self.file_path, 1, 'This is a test file.', 10),
-                              (self.file_path, 2, 'Another line with test word.', 18)]
+        expected_locations = [
+            (self.file_path, 1, 'This is a test file.', 10),
+            (self.file_path, 2, 'Another line with test word.', 18)
+        ]
         self.assertEqual(locations, expected_locations)
 
     def test_generate_links_output_file(self):
@@ -43,22 +47,23 @@ class WordLinkGeneratorTest(unittest.TestCase):
         generator = WordLinkGenerator('test', self.test_dir, None)
 
         # Redirect stdout to capture console output
-        import sys
-        original_stdout = sys.stdout
-        sys.stdout = captured_output = StringIO()
-
-        generator.generate_links()
-
-        sys.stdout = original_stdout
+        captured_output = StringIO()
+        with redirect_stdout(captured_output):
+            generator.generate_links()
 
         console_output = captured_output.getvalue()
 
-        self.assertIn('+------------------------------+------+------------------------------+', console_output)
-        self.assertIn('|             File             | Line |             Text             |', console_output)
-        self.assertIn('+------------------------------+------+------------------------------+', console_output)
-        self.assertIn('| test_directory/test_file.txt |  1   |     This is a test file.     |', console_output)
-        self.assertIn('| test_directory/test_file.txt |  2   | Another line with test word. |', console_output)
-        self.assertIn('+------------------------------+------+------------------------------+', console_output)
+        expected_output = [
+            '+------------------------------+------+------------------------------+',
+            '|             File             | Line |             Text             |',
+            '+------------------------------+------+------------------------------+',
+            '| test_directory/test_file.txt |  1   |     This is a test file.     |',
+            '| test_directory/test_file.txt |  2   | Another line with test word. |',
+            '+------------------------------+------+------------------------------+'
+        ]
+
+        for line in expected_output:
+            self.assertIn(line, console_output)
 
 
 if __name__ == '__main__':
