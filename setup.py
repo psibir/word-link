65ec283142ba828aa052310fd384befd70c8bde6
Trevor Bloomfield <bloomfieldtm@gmail.com>
2023-05-21 23:50:40 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='wordlink',
+    version='1.0',
+    description='Word Link Generator',
+    author='Trevor Bloomfield',
+    author_email='bloomfieldtm@gmail.com',
+    packages=['word_link'],
+    install_requires=[
+        'fuzzysearch',
+        'prettytable',
+    ],
+    entry_points={
+        'console_scripts': [
+            'word-link = word_link.main:main',
+        ],
+    },
+)
